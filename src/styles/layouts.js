44abd6b62e4097f073711002cebbbfef5a60b217
Ryan Irilli <ryanirilli@gmail.com>
2018-07-30 20:40:05 -0700

    add variable for max width

--- src/styles/layouts.js
+++ src/styles/layouts.js
@@ -1,6 +1,6 @@
-import styled from 'react-emotion';
-
-export const Container = styled('div')(props => ({
-  maxWidth: '1440px',
-  margin: '0 auto'
+import styled from "react-emotion";
+import { MAX_WIDTH as maxWidth } from "./style-config";
+export const Container = styled("div")(props => ({
+  maxWidth,
+  margin: "0 auto"
 }));
--- src/styles/style-config.js
+++ src/styles/style-config.js
@@ -7,6 +7,7 @@ const SECONDARY_TEXT_COLOR = Color(PRIMARY_TEXT_COLOR)
   .hex();
 
 export const BASE_SPACING_UNIT = 4;
+export const MAX_WIDTH = 1440;
 
 export const COLORS = {
   headings: PRIMARY_TEXT_COLOR,
@@ -17,7 +18,7 @@ const BREAKPOINTS = {
   small: 0,
   medium: 768,
   large: 1080,
-  xLarge: 1440
+  xLarge: MAX_WIDTH
 };
 
 export const MQ = Object.keys(BREAKPOINTS).reduce((accumulator, label) => {
