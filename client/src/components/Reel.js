4d2915ae1717e18e479e74ffde40b80d88f72c00
Ryan Irilli <ryanirilli@gmail.com>
2018-09-12 14:32:00 -0700

    fix play icon

--- client/src/components/Reel.js
+++ client/src/components/Reel.js
@@ -1,6 +1,5 @@
 // @flow
 import * as React from "react";
-import PlayButton from "../images/play-button.svg";
 import styled from "react-emotion";
 
 const Container = styled("div")`
@@ -11,7 +10,7 @@ const Container = styled("div")`
   border-radius: 100px;
 `;
 
-const Play = styled("img")`
+const Play = styled("svg")`
   width: 20px;
   margin: 0 4px 0 0;
 `;
@@ -22,7 +21,13 @@ export default class Reel extends React.Component<Props> {
   render() {
     return (
       <Container>
-        <Play src={PlayButton} />
+        <Play viewBox="0 0 41.999 41.999">
+          <path
+            d="M36.068,20.176l-29-20C6.761-0.035,6.363-0.057,6.035,0.114C5.706,0.287,5.5,0.627,5.5,0.999v40
+	c0,0.372,0.206,0.713,0.535,0.886c0.146,0.076,0.306,0.114,0.465,0.114c0.199,0,0.397-0.06,0.568-0.177l29-20
+	c0.271-0.187,0.432-0.494,0.432-0.823S36.338,20.363,36.068,20.176z M7.5,39.095V2.904l26.239,18.096L7.5,39.095z"
+          />
+        </Play>
         Reel
       </Container>
     );
